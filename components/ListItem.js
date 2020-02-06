4463dd7c1d5fff5e316ca03eb6ae23ee0cf6f38d
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-02-05 20:50:00 -0800

    Removed Sample Code Files

---
+++ components/ListItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ListItem = ({ name }) => (
+  <View>
+    <Text style={{ fontSize: 35 }}>ListItem {name}</Text>
+  </View>
+);
+
+export default ListItem;
