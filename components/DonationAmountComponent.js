b009a56f335650c5da52daadcf5e6308e7c32dca
jasminekaur95 <kjasmine2810@gmail.com>
2020-02-07 23:21:14 -0800

    Added DonationAmountComponent & DonateScreen Logic

---
+++ components/DonationAmountComponent.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Text, View, Button, TextInput, StyleSheet } from "react-native";
+
+const DonationAmountComponent = ({
+  currency,
+  initialAmount,
+  question,
+  onPressIncrement,
+  onPressDecrement,
+  onChangeDonationValue
+}) => (
+  <View style={styles.mainContainer}>
+    <Text>{question}</Text>
+
+    <View style={styles.donationContainer}>
+      <Button title="-" onPress={onPressDecrement} />
+      <Text>{currency}</Text>
+      <TextInput
+        style={styles.donationInput}
+        editable
+        keyboardType="numeric"
+        defaultValue={initialAmount}
+        onChangeText={newValue => onChangeDonationValue(newValue)}
+      />
+      <Button title="+" onPress={onPressIncrement} />
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  donationContainer: {
+    flexDirection: "row",
+    backgroundColor: "#0f0",
+    width: "100%",
+    justifyContent: "center"
+  },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#f00",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  donationInput: {
+    width: 200
+  }
+});
+export default DonationAmountComponent;
--- screens/App/Donate/DonateScreen.js
+++ screens/App/Donate/DonateScreen.js
@@ -1,12 +1,51 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button, StyleSheet } from "react-native";
+import DonationAmountComponent from "../../../components/DonationAmountComponent";
 
 export class DonateScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initialAmount: 200,
+      question: "How much would you like to Donate?",
+      currency: "$"
+    };
+  }
+
+  // Increment Donation by 1
+  handleIncrementDonation = () => {
+    this.setState({
+      initialAmount: this.state.initialAmount + 1
+    });
+  };
+
+  // Decrement Donation by 1
+  handleDecrementDonation = () => {
+    this.setState({
+      initialAmount: this.state.initialAmount - 1
+    });
+  };
+
+  // If user decides to type in the Donation Amount
+  handleDonationChange = newValue => {
+    this.setState({
+      initialAmount: parseInt(newValue)
+    });
+  };
   render() {
+    // Destructuring the State
+    const { initialAmount, question, currency } = this.state;
     return (
-      <View>
-        <Text> Donate Screen </Text>
-        <button title="Click to Donate" />
+      <View style={styles.container}>
+        <DonationAmountComponent
+          question={question}
+          initialAmount={initialAmount.toString()}
+          currency={currency}
+          onPressIncrement={this.handleIncrementDonation}
+          onPressDecrement={this.handleDecrementDonation}
+          onChangeDonationValue={this.handleDonationChange}
+        />
       </View>
     );
   }
@@ -16,4 +55,11 @@ DonateScreen.navigationOptions = {
   title: "Donate"
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    textAlign: "center"
+  }
+});
 export default DonateScreen;
