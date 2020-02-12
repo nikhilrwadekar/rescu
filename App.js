8d111b07e3937bd2ce728ba68808431fa31e3a5b
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-02-12 13:02:46 -0800

    Added Tap Anywhere to Dissmiss Keyboard

--- App.js
+++ App.js
@@ -2,7 +2,14 @@ import { AppLoading } from "expo";
 import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 import React, { useState } from "react";
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import AppNavigator from "./navigation/AppNavigator";
@@ -20,10 +27,12 @@ export default function App(props) {
     );
   } else {
     return (
-      <View style={styles.container}>
-        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
+      <DismissKeyboard>
+        <View style={styles.container}>
+          {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      </DismissKeyboard>
     );
   }
 }
@@ -61,3 +70,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
   }
 });
+
+const DismissKeyboard = ({ children }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
