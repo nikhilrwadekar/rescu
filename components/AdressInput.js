d7c87641f08bfd0aa0ea2787a7083d942d9bb33d
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-03-05 23:50:37 -0800

    Screen Updates + Fixes

--- components/AdressInput.js
+++ components/AdressInput.js
@@ -1,32 +1,64 @@
 import React from "react";
-import {View} from "react-native";
+import { View, AsyncStorage } from "react-native";
 import AppInput from "./AppInput";
 import ProvinceSelector from "./ProvinceSelector";
 
-export class AddressInput extends React.Component{
-    state = {
-        streetName: '',
-        cityName:''
-      };
+export class AddressInput extends React.Component {
+  state = {
+    streetName: "",
+    cityName: ""
+  };
 
-      render(){
-        const { streetName, cityName} = this.state;
-        return(
-            <View style={{ flex: 1, paddingRight: 40, paddingLeft: 40 }}>
-                <AppInput
-                label="StreetName"
-                value={streetName}
-                onChange={streetName => this.setState({ streetName })}
-                />
-                <AppInput
-                label="City"
-                value={cityName}
-                onChange={cityName => this.setState({ cityName })}
-                />
+  _storeData = async () => {
+    try {
+      console.log("Trying to store data..");
+      await AsyncStorage.setItem("AddressInput", JSON.stringify(this.state));
+    } catch (error) {
+      // Error saving data
+      console.log(error);
+    }
+  };
 
-                <ProvinceSelector />
-        </View>
-        );
+  _retrieveData = async () => {
+    try {
+      console.log("Trying to retrieve data..");
+      const AddressInputState = await AsyncStorage.getItem("AddressInput");
+      if (AddressInputState !== null) {
+        // We have data!!
+        console.log(AddressInputState);
+        this.setState(JSON.parse(AddressInputState));
       }
+    } catch (error) {
+      // Error retrieving data
+    }
+  };
+
+  async componentDidMount() {
+    this._retrieveData();
+  }
+
+  async componentDidUpdate() {
+    this._storeData();
+  }
+
+  render() {
+    const { streetName, cityName } = this.state;
+    return (
+      <View style={{ paddingRight: 40, paddingLeft: 40 }}>
+        <AppInput
+          label="StreetName"
+          value={streetName}
+          onChange={streetName => this.setState({ streetName })}
+        />
+        <AppInput
+          label="City"
+          value={cityName}
+          onChange={cityName => this.setState({ cityName })}
+        />
+
+        <ProvinceSelector />
+      </View>
+    );
+  }
 }
 export default AddressInput;
--- screens/Auth/PreferencesScreenTwo.js
+++ screens/Auth/PreferencesScreenTwo.js
@@ -1,13 +1,67 @@
 import React, { Component } from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
-
+import SelectMultiple from "react-native-select-multiple";
+import AdditionalSkillComponent from "../../components/AdditionalSkillComponent";
+import AcceptTermsConditionsComponent from "../../components/AcceptTermsConditionsComponent";
 export default class PreferencesScreenTwo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      termsCheck: false,
+      volunteeringTypes: [
+        "Driving",
+        "Swimming",
+        "Medical Assistance",
+        "Lifting Weight",
+        "Cooking",
+        "Babysitting",
+        "Petsitting",
+        "Elderly Care"
+      ]
+    };
+  }
+
+  // If Volunteering Options Change..
+  onSelectionsChange = selectedVolunteeringTypes => {
+    this.setState({ selectedVolunteeringTypes });
+  };
+
+  handleAdditionalSkillOrService = val => {
+    this.setState({ additionalSkill: val });
+  };
+
+  handleTermsAndConditionsCheckChange = () =>
+    this.setState({ termsCheck: !this.state.termsCheck });
+
   render() {
     const { navigation } = this.props;
-
+    const { volunteeringTypes, termsCheck } = this.state;
     return (
       <View>
-        <Text> Preferences - Type + Additional Skill </Text>
+        <Text>Types of Volunteering</Text>
+
+        {/* Multiple Select for Volunteering Types */}
+        <SelectMultiple
+          items={volunteeringTypes}
+          selectedItems={this.state.selectedVolunteeringTypes}
+          onSelectionsChange={this.onSelectionsChange}
+        />
+
+        {/* Additional Skill Section */}
+        <AdditionalSkillComponent
+          additionalSkillTextLabel="Additional Skill or Service"
+          onGetText={this.handleAdditionalSkillOrService}
+        />
+
+        {/* Accept Terms And Conditions */}
+        <AcceptTermsConditionsComponent
+          textAcceptTermsConditions="I agree to the terms and conditions"
+          checkboxValue={termsCheck}
+          onCheckboxValueChange={this.handleTermsAndConditionsCheckChange}
+        />
+
+        {/* Save and Continue */}
         <Button
           title="Save"
           onPress={() => {
