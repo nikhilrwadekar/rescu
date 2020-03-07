abdf13cfcfc2292d0814a1925c7d6252654438ca
jasminekaur95 <kjasmine2810@gmail.com>
2020-03-07 14:59:12 -0800

    Try

--- components/BaseInput.js
+++ components/BaseInput.js
@@ -1,16 +1,18 @@
 import React from "react";
-import {View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    baseInput: {
-      paddingBottom:10
-    }
-  });
-  
-  const BaseInput = ({ children, label }) => (
-    <View>
-      <Text style={styles.baseInput}>{label}</Text>
-      {children}
-    </View>
-  );
-  export default BaseInput;
+  baseInput: {
+    height: 30,
+    marginTop: 20,
+    fontSize: 20
+  }
+});
+
+const BaseInput = ({ children, label }) => (
+  <View>
+    <Text style={styles.baseInput}>{label}</Text>
+    {children}
+  </View>
+);
+export default BaseInput;
