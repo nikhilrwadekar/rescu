6f4149f432fbaf13869d7225b4133efec400b5fa
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-03-20 17:54:58 -0700

    Resolved Conflicts from Staging:

--- screens/App/Notifications/NotificationScreen.js
+++ screens/App/Notifications/NotificationScreen.js
@@ -48,21 +48,6 @@ export default class NotificationScreen extends Component {
   };
 
   render() {
-<<<<<<< HEAD
-    return (
-      <View style={styles.carouselContainer}>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          <ImageCarousel txt={"jkk"} imageUri={pic} />
-          <ImageCarousel txt={"jkk"} imageUri={pic} />
-          <ImageCarousel txt={"jkk"} imageUri={pic} />
-        </ScrollView>
-
-        {/* <AvailabilityTimePreferenceComponent
-          timeLabel={"Start Time"}
-          timeTextPlaceHolder={"8.00"}
-          onPressTime={"vv"}  
-        /> */}
-=======
     // Deconstruct State!
     const { reliefCentersWithRequests, tasks } = this.state;
 
@@ -96,7 +81,6 @@ export default class NotificationScreen extends Component {
               { name, location }
             );
           })}
->>>>>>> 86351bdbc0d0df4f54be96d740ce28ac896d50be
       </View>
     );
   }
