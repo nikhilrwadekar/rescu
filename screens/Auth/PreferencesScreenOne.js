b4b7b7d5dc00cb6e6762fc9a31332cd8232149ba
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-03-22 02:05:38 -0700

    Added Splash; Changed Color; Componentized PreferencesScreen One&Two

--- screens/Auth/PreferencesScreenOne.js
+++ screens/Auth/PreferencesScreenOne.js
@@ -1,27 +1,8 @@
 import React, { Component } from "react";
 
-// React Native
-import {
-  Text,
-  StyleSheet,
-  View,
-  Button,
-  AsyncStorage,
-  SafeAreaView
-} from "react-native";
-import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
-
 // Custom Components
-import AddressInput from "../../components/AdressInput";
-import PostalCode from "../../components/PostalCode";
-import ButtonLink from "../../components/ButtonLink";
-import TimeAvailability from "../../components/TimeAvailability";
-// import DateTimeModal from "../../components/DateModal";
-
-import DateTimePicker from "@react-native-community/datetimepicker";
-import Modal from "react-native-modal";
-import { Card } from "react-native-elements";
-
+import PreferencesScreenOneComponent from "../../components/PreferencesScreenOneComponent";
+import { SafeAreaView } from "react-native";
 export default class PreferencesScreenOne extends Component {
   constructor(props) {
     super(props);
@@ -93,124 +74,29 @@ export default class PreferencesScreenOne extends Component {
   // Render Function
   render() {
     const { navigation } = this.props;
-    const { preference, timePreferences } = this.state;
+    const {
+      preference,
+      timePreferences,
+      isModalVisible,
+      currentModalLabel
+    } = this.state;
 
     return (
       <SafeAreaView>
-        <ScrollView>
-          {/* PreferencesScreenOne */}
-          <Text style={styles.textHeader}> Preferences </Text>
-
-          <AddressInput />
-          <PostalCode />
-
-          <ButtonLink
-            text="Next"
-            onPress={() => {
-              navigation.navigate("PreferencesScreenTwo");
-            }}
-          />
-
-          {/* Select Preference */}
-          <TimeAvailability
-            setPreference={this.handleSetPreference}
-            preference={preference}
-          />
-
-          {/* If Preference is 'preferred' Show DateTimePicker */}
-          {preference == "preferred" && (
-            <View>
-              {/* Map Date Modal */}
-              {timePreferences.map((timePreference, index) => (
-                // Preference - Date & Time
-                <View key={timePreference.key}>
-                  {/* Buttons for Date, Start Time, and End Time */}
-                  <View
-                    style={{
-                      flex: 1,
-                      flexDirection: "row",
-                      justifyContent: "center"
-                    }}
-                  >
-                    <Button
-                      title={
-                        timePreference.date
-                          ? `${new Date(timePreference.date).toDateString()} `
-                          : new Date().toDateString()
-                      }
-                      onPress={() =>
-                        this.toggleModal(timePreference.key, "Date")
-                      }
-                    />
-                    <Button
-                      title={new Date(
-                        timePreference.start_time
-                      ).toLocaleTimeString()}
-                      onPress={() =>
-                        this.toggleModal(timePreference.key, "Start Time")
-                      }
-                    />
-                    <Button
-                      title={new Date(
-                        timePreference.end_time
-                      ).toLocaleTimeString()}
-                      onPress={() =>
-                        this.toggleModal(timePreference.key, "End Time")
-                      }
-                    />
-                  </View>
-
-                  {/* The Modal that Conditionally Renders based on which Button is clicked */}
-                  <Modal
-                    onBackdropPress={this.toggleModal}
-                    isVisible={this.state.isModalVisible}
-                    style={{ flex: 1 }}
-                  >
-                    {/* View inside the Modal! */}
-                    <View>
-                      <Card>
-                        {/* Dynamic Label - Date, Start Time OR End Time */}
-                        <Text>{this.state.currentModalLabel}</Text>
-                        <DateTimePicker
-                          testID="dateTimePicker"
-                          value={
-                            new Date(
-                              this.state.currentModalLabel == "Date"
-                                ? timePreference.date
-                                : this.state.currentModalLabel == "Start Time"
-                                ? timePreference.start_time
-                                : timePreference.end_time
-                            )
-                          }
-                          mode={
-                            this.state.currentModalLabel == "Date"
-                              ? "date"
-                              : "time"
-                          }
-                          display="default"
-                          onChange={this.onChange}
-                        />
-                      </Card>
-                    </View>
-                  </Modal>
-                </View>
-              ))}
-
-              {/* Button to Add More Preferences */}
-              <Button title="Add More" onPress={this.addMorePreferences} />
-            </View>
-          )}
-        </ScrollView>
+        <PreferencesScreenOneComponent
+          addMorePreferences={this.addMorePreferences}
+          preference={preference}
+          isModalVisible={isModalVisible}
+          handleSetPreference={this.handleSetPreference}
+          timePreferences={timePreferences}
+          toggleModal={this.toggleModal}
+          currentModalLabel={currentModalLabel}
+          onChange={this.onChange}
+          onPressNext={() => {
+            this.props.navigation.navigate("PreferencesScreenTwo");
+          }}
+        />
       </SafeAreaView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  textHeader: {
-    textAlign: "center",
-    marginTop: 20,
-    fontSize: 25,
-    fontFamily: "Quicksand-Medium"
-  }
-});
